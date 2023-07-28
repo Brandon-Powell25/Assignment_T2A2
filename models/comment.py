d5646b8dc662a81454d063df78fab84ca0b53483
Brandon-Powell25 <14297@coderacademy.edu.au>
2023-07-28 13:08:47 +1000

    Add coment model and Grud comments

--- models/comment.py
+++ models/comment.py
@@ -2,23 +2,25 @@ from init import db, ma
 from marshmallow import fields
 
 class Comment(db.Model):
+    # Table name
     __tablename__ = 'comments'
 
     id = db.Column(db.Integer, primary_key=True)
     comment = db.Column(db.Text)
 
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    task_id = db.Column(db.Integer, db.ForeignKey('tasks.id'), nullable=False)
 
     user = db.relationship('User', back_populates='comments')
     tasks = db.relationship('Task', back_populates='comments')
 
 class CommentSchema(ma.Schema):
     user = fields.Nested('UserSchema', only=['name', 'email'])
-    task = fields.Nested('TaskSchema', exclude=['comments'])
+    tasks = fields.Nested('TaskSchema', exclude=['comments'])
 
     class Meta:
         fields = ('id', 'comment', 'task', 'user')
         ordered = True
 
-commment_schema = CommentSchema()
-comments_schema = CommentSchema(many=True)
+comment_schema = CommentSchema()
+comments_Schema = CommentSchema(many=True)
